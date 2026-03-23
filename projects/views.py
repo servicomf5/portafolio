ebe68accb5c43d9fdf93b31ef40f57e2d1f4733e
Eduardo Munoz <eduardomunoz.trabajo@gmail.com>
2026-03-23 12:42:26 -0300

    Corregir vista portfolio para pasar profile, fixear calculo experiencia en about

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Project, CaseStudy
+from portfolio.models import Profile
 
 
 class PortfolioView(TemplateView):
@@ -10,6 +11,7 @@ class PortfolioView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["profile"] = Profile.objects.first()
         context["projects"] = Project.objects.all()
         context["case_study"] = CaseStudy.objects.first()
         return context
